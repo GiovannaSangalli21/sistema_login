8ba1c6489ac4edc4ca00b269f93486ca862294d2
GiovannaSangalli21 <giovannasangalli55617@gmail.com>
2026-04-17 11:25:54 -0300

    feat:adicionar sitema de login criados durante a aula

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+
+app = Flask (__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    user = request.form.get ('user')
+    password = request.form.get('password')
+    
+    if user == "sangalli" and password == "1234":
+        return "ACESSO OK"
+    else: 
+        return "ACESSO NAO ENCONTRADO"
+    
+if __name__ == '__main__':
+    app.run("0.0.0.0",port=8000)
